e84434fbb6867e4e4a817d3fdc81a9e881850981
TarequeBashar <ovitareque@gmail.com>
2020-07-22 20:23:31 +0600

    Final

---
+++ JungleRunner 2d/Assets/code/player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    public float speed ;
+
+    private Rigidbody2D rigidBody ;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidBody.velocity = new Vector2(speed , rigidBody.velocity.y);
+    }
+}
